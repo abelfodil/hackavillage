853884513cf680099d7df574b83cfbff56f3aa1f
yoangau <31855017+yoangau@users.noreply.github.com>
2018-01-20 19:05:03 -0500

    added types

---
+++ src/Components/Common/LineCharts.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+
+interface ChartsProps {
+    data: Array<object>;
+}
+
+class Charts extends React.Component<ChartsProps> {
+    render() {
+        return (
+
+            <LineChart
+                width={730}
+                height={250}
+                data={this.props.data}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}
+            >
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <Tooltip/>
+                <Legend/>
+                <Line type="monotone" dataKey="pv" stroke="#8884d8"/>
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d"/>
+            </LineChart>
+        );
+    }
+}
+
+export default Charts;
--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -2,6 +2,17 @@ import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
+import Charts from './Common/LineCharts';
+
+const data = [
+    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
 
 class LandingPage extends React.Component {
     render() {
@@ -13,6 +24,7 @@ class LandingPage extends React.Component {
                         <Button id="register" color="contrast">Register</Button>
                     </Toolbar>
                 </AppBar>
+                <Charts data={data}/>
                 <h2><strong> About Hackavillage </strong></h2>
                 <p>Hackavillage is an interactive platform aiming to improve the social environment of cities around the
                     world through its contributors by using dividends. It offers a large range of solutions for smart
