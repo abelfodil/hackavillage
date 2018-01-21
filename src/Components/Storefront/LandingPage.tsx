a2ea40b8cf1b478c791869b59046876dc9115c39
Anes Belfodil <ans.belfodil@gmail.com>
2018-01-20 23:06:25 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Components/Storefront/LandingPage.tsx

--- src/Components/Storefront/LandingPage.tsx
+++ src/Components/Storefront/LandingPage.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react';
-import PieCharts  from '../Common/PieCharts';
+import * as React  from 'react';
+import PieCharts   from '../Common/PieCharts';
+import Avatar      from 'material-ui/Avatar';
+import ProgressBar from '../Common/ProgressBar';
 
-const data1 = [{name: 'North America', value: 40}, {name: 'Group B', value: 300},
-    {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
-    {name: 'Group E', value: 278}, {name: 'Group F', value: 189}];
-
-const data2 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
-    {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
-    {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
+const data1 = [{name: 'BIXI', value: 30}, {name: 'Energy', value: 25},
+    {name: 'STM', value: 40}, {name: 'Other', value: 5}];
 
 class LandingPage extends React.Component {
     render() {
         return (
             <div id="landpage">
-                <PieCharts data1={data1} data2={data2}/>
                 <h2><strong> About Hackavillage </strong></h2>
                 <p>Hackavillage is an interactive platform aiming to improve the social environment of cities around the
                     world through its contributors by using dividends. It offers a large range of solutions for smart
@@ -31,13 +27,22 @@ class LandingPage extends React.Component {
                     financial goal. The optic is to make the organizations save money while they give back to their
                     users in a form of social rewarding at an affordable price.</p>
                 <h2><strong>Here is an example</strong></h2>
-                <img src={require('../../Resources/garnier.jpg')} alt="David, un utilisateur quelconque"/>
-                <p>David is studying Computer Engineering at Polytechnique Montreal and subscribed to Hackavillage a
-                    year ago. He uses a BIXI from the non-profit organization every day possible to go to school. While
-                    he gains points and rewards like rebate on its monthly pass for doing so, he helps the city solve
-                    its traffic and CO2 problems. On the other side, BIXI Montreal can see the abstract data converted
-                    into charts and graph to help see the progress towards its goal. This data can be projected to help
-                    adjust the pricing and rebate to its product.</p>
+                <div>
+                    <Avatar
+                        alt="David, un utilisateur quelconque"
+                        src={require('../../Resources/garnier.jpg')}
+                    />
+                    <PieCharts data1={data1}/>
+                    <ProgressBar value={70}/>
+                    <p>David is studying Computer Engineering at Polytechnique Montreal and subscribed to Hackavillage a
+                        year ago. He uses a BIXI from the non-profit organization every day possible to go to school.
+                        While he gains points and rewards like rebate on its monthly pass for doing so, he helps the
+                        city
+                        solve its traffic and CO2 problems. On the other side, BIXI Montreal can see the abstract data
+                        converted into charts and graph to help see the progress towards its goal. This data can be
+                        projected to
+                        help adjust the pricing and rebate to its product.</p>
+                </div>
                 <h2><strong>Future goal</strong></h2>
                 <p> We already have a good working product. While we continually work on improving the performance and
                     the usability of our platform, we are currently working on expanding the extend of the social system
